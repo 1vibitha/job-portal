ff073fe8b9f5fab8260f4b095ec61e323f2cfb1b
vibitha <vibithavijayan6@gmail.com>
2025-11-18 19:52:49 +0530

    job listing and search

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,5 +1,49 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { jobsData } from "../assets/assets";
 
 
 export const AppContext = createContext() 
 
+export const AppContextProvider = (props) => {
+
+
+   const [searchFilter,setSearchFilter] = useState({
+        title:'',
+        location:''
+   }) 
+
+   const [isSearched,setIsSearched] = useState(false) 
+
+   const [jobs, setJobs] = useState([])
+
+   // function to fetch jobs
+
+   const fetchJobs = async () =>
+   {
+
+        setJobs(jobsData)
+
+   }
+
+   useEffect(()=>
+    {
+        
+        fetchJobs()
+
+    },[])
+
+    const value = {
+        searchFilter,setSearchFilter,
+        isSearched,setIsSearched,
+        jobs,setJobs,
+    }
+
+    return (
+        <AppContext.Provider value={value} >
+
+            {props.children}
+
+        </AppContext.Provider>
+    )
+
+}
